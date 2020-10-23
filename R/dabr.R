98260698a4fdbf9453491cf1d5433f5f97751107
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-23 12:28:11 +0100

    Rename select_query_mysql to select

--- R/dabr.R
+++ R/dabr.R
@@ -4,7 +4,7 @@
 #'
 #' @param dbname database name
 #' @param user username of database owner
-#' @param password password [default: \code{NULL}]
+#' @param password password (default: \code{NULL})
 #' @param host database host, it can be local (default) or remote
 #' @param port database port
 #'
@@ -62,25 +62,33 @@ close_conn.default <- function(conn, ...) {
   stop("Invalid connection object")
 }
 
-#' Execute select query
+#' Execute \code{SELECT} query
 #'
-#' Retrieves records that meet the criteria declared in the query
+#' @param conn \code{MariaDBConnection} connection object.
+#' @param ... Optional parameters.
 #'
-#' @param conn \code{MariaDBConnection} connection object
-#' @param query select query
-#' @param quiet boolean flag to hide status messages
+#' @return Data frame containing the selected records.
+#' @rdname select
+#' @export
 #'
-#' @return data frame containing the selected records
+#' @family DB functions
+select <- function(conn, ...) {
+  UseMethod("select", conn)
+}
+
+#' @param query \code{SELECT} query.
+#' @param quiet Boolean flag to hide status messages.
+#'
+#' @rdname select
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' conn <- open_conn_mysql("sys", "root")
-#' out <- select_query_mysql(conn, "SELECT variable, value FROM sys_config")
+#' out <- select(conn, "SELECT variable, value FROM sys_config")
 #' close_conn(conn)
 #' }
-#' @family DB functions
-select_query_mysql <- function(conn, query, quiet = FALSE) {
+select.MariaDBConnection <- function(conn, query, quiet = FALSE, ...) {
   # Verify that the query has a SELECT token
   if (!("SELECT" %in% unlist(strsplit(toupper(query), " "))))
     stop("Your query does not look like a valid SELECT query!")
@@ -92,7 +100,7 @@ select_query_mysql <- function(conn, query, quiet = FALSE) {
   rs <- RMariaDB::dbSendQuery(conn, query)
   # Fetch records
   records <- RMariaDB::dbFetch(rs)
-  # Show query
+  # Show query results
   if (!quiet)
     message(paste0("Results:\n", nrow(records), " records were found."))
   # Clear the result
@@ -128,7 +136,7 @@ select_all <- function(conn, ...) {
 #' }
 select_all.MariaDBConnection <- function(conn, table, quiet = FALSE, ...) {
   query <- paste0("SELECT * FROM ", table)
-  return(ageR::select_query_mysql(conn, query, quiet))
+  return(dabr::select(conn, query, quiet))
 }
 
 #' Check if \code{conn} is an object with class \code{MariaDBConnection}
