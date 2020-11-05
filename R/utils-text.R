4559393b8259cd753b63054edbef34021cb554b1
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-11-05 17:24:36 +0000

    Add documentation and examples

--- R/utils-text.R
+++ R/utils-text.R
@@ -1,9 +1,31 @@
+#' Add single quotes
+#'
+#' Add single quotes to string.
+#'
+#' @param str String.
+#'
+#' @return String surrounded by single quotes
 #' @export
+#'
+#' @examples
+#' dabr::quote("A")
 quote <- function(str) {
   return(paste0("'", str, "'"))
 }
 
+#' Combine attributes
+#'
+#' Combine attributes from a vector of strings.
+#'
+#' @param ... Strings.
+#'
+#' @return Combined string with all the attributes.
 #' @export
+#'
+#' @examples
+#' dabr::attributes("A", "B", "C")
+#' dabr::attributes(c("A", "B", "C"))
+#' dabr::attributes(c("A", "B", "C"), "D", "E", "F")
 attributes <- function(...) {
-  return(paste0(..., collapse = ", "))
+  return(paste(c(...), sep = ", ", collapse = ", "))
 }
