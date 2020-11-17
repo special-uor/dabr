e2c767a0de2b196aaafe5287e2cef5200bda8a52
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-11-17 20:27:03 +0000

    Reorganise functions in alphabetical order

--- R/dabr.R
+++ R/dabr.R
@@ -1,6 +1,185 @@
 #' @keywords internal
 "_PACKAGE"
 
+#' Close connection to database
+#'
+#' @param conn DB connection object.
+#' @param ... Optional parameters.
+#'
+#' @rdname close_conn
+#' @export
+#'
+#' @family DB functions
+close_conn <- function(conn, ...) {
+  UseMethod("close_conn", conn)
+}
+
+#' @rdname close_conn
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' conn <- open_conn_mysql("sys", "root")
+#' close_conn(conn)
+#' }
+close_conn.MariaDBConnection <- function(conn, ...) {
+  RMariaDB::dbDisconnect(conn)
+}
+
+#' @rdname close_conn
+#' @export
+close_conn.default <- function(conn, ...) {
+  stop("Invalid connection object")
+}
+
+#' Get attributes of a table
+#'
+#' @inheritParams close_conn
+#'
+#' @export
+#'
+#' @rdname get_attr
+get_attr <- function(conn, ...) {
+  UseMethod("get_attr", conn)
+}
+
+#' @param name Table name.
+#'
+#' @return List of attributes for table \code{name}.
+#' @export
+#'
+#' @rdname get_attr
+get_attr.MariaDBConnection <- function(conn, name, ...) {
+  tryCatch({
+    return(RMariaDB::dbListFields(conn = conn, name = name))
+  }, error = function(e) {
+    stop(conditionMessage(e))
+  })
+}
+
+#' Execute \code{INSERT} query
+#'
+#' @param conn DB connection object.
+#' @param ... \code{INSERT} query and optional parameters.
+#'
+#' @rdname insert
+#' @export
+#'
+#' @family DB functions
+insert <- function(conn, ...) {
+  UseMethod("insert", conn)
+}
+
+#' @param quiet Boolean flag to hide status messages.
+#'
+#' @rdname insert
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' conn <- open_conn_mysql("sys", "root")
+#' query <- paste0(
+#'   "INSERT INTO sys_config (variable, value, set_time, set_by) VALUES ",
+#'   "('test_var', 999, '", Sys.time(), "', NULL)"
+#' )
+#' out <- insert(conn, query)
+#' close_conn(conn)
+#' }
+insert.MariaDBConnection <- function(conn, ..., quiet = FALSE) {
+  query <- paste(unlist(lapply(c(...), trimws)), collapse = " ")
+  # Verify that the query has a INSERT token
+  if (!("INSERT" %in% unlist(strsplit(toupper(query), " "))))
+    stop("Your query does not look like a valid INSERT query!")
+  # Show query
+  if (!quiet)
+    message(paste0("Executing: \n", query))
+
+  tryCatch({
+    # Send query
+    rs <- RMariaDB::dbSendQuery(conn, query)
+    rows <- RMariaDB::dbGetRowsAffected(rs)
+    # Show query results
+    if (!quiet)
+      if (rows == 0) {
+        message("\nResults: No records were inserted.")
+      } else {
+        message("\nResults: ", rows, " record",
+                ifelse(rows > 1, "s were ", " was "),
+                "inserted.")
+      }
+
+    # Clear the result
+    RMariaDB::dbClearResult(rs)
+  }, error = function(e) {
+    stop(conditionMessage(e))
+  })
+}
+
+#' Verify connection
+#'
+#' Verify if connection object is still valid, is connected to the database
+#' server.
+#'
+#' @inheritParams close_conn
+#'
+#' @return Connection status.
+#' @export
+#'
+#' @rdname is.connected
+is.connected <- function(conn,  ...) {
+  UseMethod("is.connected", conn)
+}
+
+#' @export
+#'
+#' @rdname is.connected
+is.connected.MariaDBConnection <- function(conn, ...) {
+  RMariaDB::dbIsValid(conn)
+}
+
+#' List tables
+#'
+#' @param conn DB connection object.
+#' @param ... Optional parameters.
+#'
+#' @rdname list_tables
+#' @export
+#'
+#' @family DB functions
+list_tables <- function(conn, ...) {
+  UseMethod("list_tables", conn)
+}
+
+#' @param quiet Boolean flag to hide status messages.
+#' @param attr Boolean flag to list the attributes of each table.
+#'
+#' @return If \code{quiet = TRUE} returns a list with the tables' names. If
+#'     \code{attr = TRUE} includes each attribute of the tables.
+#'
+#' @rdname list_tables
+#' @export
+list_tables.MariaDBConnection <- function(conn, quiet = FALSE, attr = TRUE, ...) {
+  tryCatch({
+    table_names <- RMariaDB::dbListTables(conn)
+    if (!attr)
+      return(table_names)
+    tables_info <- lapply(table_names, get_attr, conn = conn)
+    names(tables_info) <- table_names
+    if (!quiet) {
+      for (i in seq_along(tables_info))
+        print(knitr::kable(tables_info[[i]], col.names = table_names[i]))
+    } else {
+      out <- vector("list", length(table_names))
+      for (i in seq_along(tables_info))
+        out[[i]] <- tables_info[[i]]
+      names(out) <- table_names
+      return(out)
+    }
+  }, error = function(e) {
+    stop(conditionMessage(e))
+  })
+}
+
 #' Connect to database
 #'
 #' Uses \code{RMariaDB} to open a connection to a MySQL database.
@@ -32,37 +211,6 @@ open_conn_mysql <- function(dbname,
                               host = host,
                               port = port)
   return(conn)
-}
-
-#' Close connection to database
-#'
-#' @param conn DB connection object.
-#' @param ... Optional parameters.
-#'
-#' @rdname close_conn
-#' @export
-#'
-#' @family DB functions
-close_conn <- function(conn, ...) {
-  UseMethod("close_conn", conn)
-}
-
-#' @rdname close_conn
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' conn <- open_conn_mysql("sys", "root")
-#' close_conn(conn)
-#' }
-close_conn.MariaDBConnection <- function(conn, ...) {
-  RMariaDB::dbDisconnect(conn)
-}
-
-#' @rdname close_conn
-#' @export
-close_conn.default <- function(conn, ...) {
-  stop("Invalid connection object")
 }
 
 #' Execute \code{SELECT} query
@@ -203,152 +351,4 @@ update.MariaDBConnection <- function(conn, ..., quiet = FALSE) {
   }, error = function(e) {
     stop(conditionMessage(e))
   })
-}
-
-#' Execute \code{INSERT} query
-#'
-#' @param conn DB connection object.
-#' @param ... \code{INSERT} query and optional parameters.
-#'
-#' @rdname insert
-#' @export
-#'
-#' @family DB functions
-insert <- function(conn, ...) {
-  UseMethod("insert", conn)
-}
-
-#' @param quiet Boolean flag to hide status messages.
-#'
-#' @rdname insert
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' conn <- open_conn_mysql("sys", "root")
-#' query <- paste0(
-#'   "INSERT INTO sys_config (variable, value, set_time, set_by) VALUES ",
-#'   "('test_var', 999, '", Sys.time(), "', NULL)"
-#' )
-#' out <- insert(conn, query)
-#' close_conn(conn)
-#' }
-insert.MariaDBConnection <- function(conn, ..., quiet = FALSE) {
-  query <- paste(unlist(lapply(c(...), trimws)), collapse = " ")
-  # Verify that the query has a INSERT token
-  if (!("INSERT" %in% unlist(strsplit(toupper(query), " "))))
-    stop("Your query does not look like a valid INSERT query!")
-  # Show query
-  if (!quiet)
-    message(paste0("Executing: \n", query))
-
-  tryCatch({
-    # Send query
-    rs <- RMariaDB::dbSendQuery(conn, query)
-    rows <- RMariaDB::dbGetRowsAffected(rs)
-    # Show query results
-    if (!quiet)
-      if (rows == 0) {
-        message("\nResults: No records were inserted.")
-      } else {
-        message("\nResults: ", rows, " record",
-                ifelse(rows > 1, "s were ", " was "),
-                "inserted.")
-      }
-
-    # Clear the result
-    RMariaDB::dbClearResult(rs)
-  }, error = function(e) {
-    stop(conditionMessage(e))
-  })
-}
-
-#' List tables
-#'
-#' @param conn DB connection object.
-#' @param ... Optional parameters.
-#'
-#' @rdname list_tables
-#' @export
-#'
-#' @family DB functions
-list_tables <- function(conn, ...) {
-  UseMethod("list_tables", conn)
-}
-
-#' @param quiet Boolean flag to hide status messages.
-#' @param attr Boolean flag to list the attributes of each table.
-#'
-#' @return If \code{quiet = TRUE} returns a list with the tables' names. If
-#'     \code{attr = TRUE} includes each attribute of the tables.
-#'
-#' @rdname list_tables
-#' @export
-list_tables.MariaDBConnection <- function(conn, quiet = FALSE, attr = TRUE, ...) {
-  tryCatch({
-    table_names <- RMariaDB::dbListTables(conn)
-    if (!attr)
-      return(table_names)
-    tables_info <- lapply(table_names, get_attr, conn = conn)
-    names(tables_info) <- table_names
-    if (!quiet) {
-      for (i in seq_along(tables_info))
-        print(knitr::kable(tables_info[[i]], col.names = table_names[i]))
-    } else {
-      out <- vector("list", length(table_names))
-      for (i in seq_along(tables_info))
-        out[[i]] <- tables_info[[i]]
-      names(out) <- table_names
-      return(out)
-    }
-  }, error = function(e) {
-    stop(conditionMessage(e))
-  })
-}
-
-#' Get attributes of a table
-#'
-#' @inheritParams close_conn
-#'
-#' @export
-#'
-#' @rdname get_attr
-get_attr <- function(conn, ...) {
-  UseMethod("get_attr", conn)
-}
-
-#' @param name Table name.
-#'
-#' @return List of attributes for table \code{name}.
-#' @export
-#'
-#' @rdname get_attr
-get_attr.MariaDBConnection <- function(conn, name, ...) {
-  tryCatch({
-    return(RMariaDB::dbListFields(conn = conn, name = name))
-  }, error = function(e) {
-    stop(conditionMessage(e))
-  })
-}
-
-#' Verify connection
-#'
-#' Verify if connection object is still valid, is connected to the database
-#' server.
-#'
-#' @inheritParams close_conn
-#'
-#' @return Connection status.
-#' @export
-#'
-#' @rdname is.connected
-is.connected <- function(conn,  ...) {
-  UseMethod("is.connected", conn)
-}
-
-#' @export
-#'
-#' @rdname is.connected
-is.connected.MariaDBConnection <- function(conn, ...) {
-  RMariaDB::dbIsValid(conn)
 }
